847dd7af5ed74d754f80727c474581dbf9daa253
Herschel Pawar <pawarherschel@gmail.com>
2026-01-07 22:03:48 +0530

    final for now

--- export.sh
+++ export.sh
@@ -7,3 +7,7 @@ typst compile --format png slides.typ slides/png/slide-{0p}-of-{t}.png --feature
 typst compile --format svg slides.typ slides/svg/slide-{0p}-of-{t}.svg --features html --ppi 288
 typst compile slides.typ --features html --ppi 288 --pdf-standard ua-1
 ./touying-export.sh
+
+typst compile Introduction_Video.typ --features html --ppi 288 --pdf-standard ua-1
+typst compile Portfolio-pawar-herschel.typ --features html --ppi 288 --pdf-standard ua-1
+
